536ab2d70aca1fd0a632702b1297353e2fb6da10
Mariano Markon <mariano@camba.coop>
2020-07-11 19:26:15 -0300

    Primera iteracion mapa con marcadores mockeados

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import cadena from './images/cadena.jpeg';
 import FastaUpload from './components/FastaUpload';
+import MapSequences from './components/mapsequences/mapsequences';
 import './App.css';
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
             <h2>BIOINFORMATICA</h2>
             <p>TP final</p>
         </div>
-
+        <MapSequences/>
         <FastaUpload/>
       </header>
     </div>
